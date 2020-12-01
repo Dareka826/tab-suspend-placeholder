3091ca8a09c103db5b798fb4849d942d84a4a7cb
Rin Takanashi <dareka826@gmail.com>
2020-12-01 16:27:25 +0100

    Reformat code to look nicer + add comments

--- popup/popup.js
+++ popup/popup.js
@@ -4,17 +4,16 @@ function togglePageSuspension(url, title) {
 	let suspendedURL = browser.extension.getURL("/suspended/suspended.html");
 	let newURL = "";
 
+	// Flip URL between suspended and not-suspended version
 	if(url.startsWith(suspendedURL)) {
-		console.log("Suspended tab");
-
-		//let tabTitle = url.match(/title=([^&]*)&/)[1];
 		let tabURL = url.match(/url=(.*)/)[1];
-		newURL = tabURL;
-	} else {
-		console.log("Non-suspended tab");
-		newURL = suspendedURL + "?title=" + title + "&url=" + url;
-	}
+		newURL = decodeURI(tabURL);
+	} else
+		newURL = suspendedURL +
+			"?title=" + encodeURI(title)
+			+ "&url=" + encodeURI(url);
 
+	// Change tab to new URL
 	browser.tabs.update(
 		{ "url": newURL }
 	);
@@ -24,9 +23,12 @@ document.addEventListener("click", (e) => {
 
 	// If "Suspend/Unsuspend" button clicked
 	if(e.target.id == "toggle-current")
+		// Query current tab for data
 		browser.tabs.query({currentWindow: true, active :true}).then((x) => {
 			let tabURL = x[0].url;
 			let tabTitle = x[0].title;
+			
+			// Make a new URL using page title and URL
 			togglePageSuspension(tabURL, tabTitle);
 		});
 
