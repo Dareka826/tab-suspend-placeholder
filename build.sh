237c1032c91b831e24dc9922d21c320c901fe6ab
Rin Takanashi <dareka826@gmail.com>
2020-12-01 16:54:21 +0100

    Don't pack unnecessary files

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p build
-zip -r -FS build/tab-suspend-placeholder.xpi * --exclude '*.git'
+zip -r -FS build/tab-suspend-placeholder.xpi * --exclude '*.git' --exclude README.md --exclude 'build*'
 
