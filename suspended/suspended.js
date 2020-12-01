f5fb921f115853b8b55ec2d91a48022a58c3477e
Rin Takanashi <dareka826@gmail.com>
2020-12-01 16:31:00 +0100

    Created the suspended page

---
+++ suspended/suspended.js
@@ -0,0 +1,49 @@
+
+// If string exceeds maxlen, shorten it and add "..."
+function limitLen(text, maxlen) {
+	if(text.length <= maxlen) return text;
+	return text.substr(0, maxlen-3) + "...";
+}
+
+// Get DOM elements
+let pageTitle = document.getElementById("page-title");
+let pageURL = document.getElementById("page-url");
+let unsuspendBtn = document.getElementById("unsuspend-btn");
+
+// Max length definitions
+let maxTitleLen = Infinity;
+let maxUrlLen = Infinity;
+
+// Get page URL
+let data = window.location.href;
+
+// Extract title from URL
+pageTitle.innerText = limitLen(
+	decodeURI(
+		data.match(/\?title=(.*)&url=/)[1]
+	), maxTitleLen
+);
+
+// Set the tab title
+document.title= pageTitle.innerText;
+
+// Extract unsuspended URL form URL
+pageURL.innerText = limitLen(
+	decodeURI(
+		data.match(/&url=(.*)/)[1]
+	), maxUrlLen
+);
+
+// Add a listener to the button
+unsuspendBtn.addEventListener("click", () => {
+	try {
+		// Navigate to the unsuspended URL
+		window.location.href = pageURL.innerText;
+	} catch(e) {
+		// If an error occurs (probably TypeError for sites like
+		// about:devtools), alert it and log
+		console.log(e);
+		alert(e);
+	}
+});
+
