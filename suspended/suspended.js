d98ab955e3b1d541d9f02947dc8555441d3e080d
Rin Takanashi <dareka826@gmail.com>
2021-05-30 17:30:02 +0200

    Add option for displaying an image and make parameter parsing better

--- suspended/suspended.js
+++ suspended/suspended.js
@@ -8,6 +8,7 @@ function limitLen(text, maxlen) {
 // Get DOM elements
 let pageTitle = document.getElementById("page-title");
 let pageURL = document.getElementById("page-url");
+let pageImg = document.getElementById("page-img");
 let unsuspendBtn = document.getElementById("unsuspend-btn");
 
 // Max length definitions
@@ -20,7 +21,7 @@ let data = window.location.href;
 // Extract title from URL
 pageTitle.innerText = limitLen(
 	decodeURI(
-		data.match(/\?title=(.*)&url=/)[1]
+		data.match(/[&?]title=([^&]*)&url=/)[1]
 	), maxTitleLen
 );
 
@@ -30,9 +31,17 @@ document.title= pageTitle.innerText;
 // Extract unsuspended URL form URL
 pageURL.innerText = limitLen(
 	decodeURI(
-		data.match(/&url=(.*)/)[1]
+		data.match(/[&?]url=([^&]*)/)[1]
 	), maxUrlLen
 );
+
+// Add image if one is specified
+let imgURL = data.match(/[&?]img=([^&]*)/);
+if(imgURL != null) {
+	let img = document.createElement("img");
+	pageImg.appendChild(img);
+	img.src = decodeURI(imgURL[1]);
+}
 
 // Add a listener to the button
 unsuspendBtn.addEventListener("click", () => {
